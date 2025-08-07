1fdebdad5c04f4c60315aad1fb00ffc7789ba8c4
Federico Casas <casasfede175@gmail.com>
2025-08-07 02:41:36 -0300

    Corrijo archivo app.js con la lógica del sorteo

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    mostrarLista();
+}
+
+function mostrarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar lista actual
+
+    amigos.forEach(function (nombre) {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Primero debes agregar amigos.");
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indice];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
